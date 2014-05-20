a8c4e243621dadf6bf9a1e0cb61f174f71c91d2d
furukawa <s11544@salesio-sp.ac.jp>
2014-05-20 19:17:13 +0900

    change the h-star of selection point

--- A-ster.py
+++ A-ster.py
@@ -12,9 +12,9 @@ from heapq import heappush, heappop
 from copy import deepcopy,copy
 
 
-LIMIT_SELECTION = 3 #選択上限、適宜変更
+LIMIT_SELECTION = 5 #選択上限、適宜変更
 SELECTON_RATE = 1 #選択コストレート、適宜変更
-EXCHANGE_RATE = 1 #交換コストレート、適宜変更
+EXCHANGE_RATE = 2 #交換コストレート、適宜変更
 
 class Node :
     def __init__ (self, board, selection):
@@ -116,195 +116,196 @@ def caliculate_cost (operations): #現在のoperationsのコストを返す
         pair = pair[1]
     return cost
 
-def selection_h_star(table, selected_node):
-    distance_min = 999999999
-    next_nodes = selected_node.get_next_nodes() #looking_nodeに隣接するノードたち(上下左右)を辞書型でnext_nodesに追加
-    for direction in ["R","L","U","D"]: #中身全部取り出すぜー
-        node = next_nodes[direction]
-        if node.board != None:
-            #distance_average += distance_to_goal(table,node.board)
-            #distance_average /= 2
-            distance_min = min(distance_min, distance_to_goal(table, node.board))
-    return (distance_min / min(SELECTON_RATE, EXCHANGE_RATE)) * max(SELECTON_RATE, EXCHANGE_RATE) -1
+def selection_h_star(x,y):
+    goal_point = distance_table[(x,y)]
+    distance = abs(goal_point[0]-x)+abs(goal_point[1]-y)
+    distance = distance*1.0
+    if distance == 0 :
+      distance = 0.9
+    else :
+      distance = (1 / distance)
+    return int(distance*50)
+
 
 def split(img, columns, rows):
-  images = []
-  # shapeで画像サイズを取れるが、[高さ, 幅, 3]の順番になっているので注意
-  fullWidth = img.shape[1]
-  fullHeight = img.shape[0]
-  splitWidth = fullWidth / columns
-  splitHeight = fullHeight / rows
-  for i in range(columns):
-    column = []
-    for j in range(rows):
-      left = splitWidth * i
-      right = left + splitWidth
-      top = splitHeight * j
-      bottom = top + splitHeight
-      # 画像を切り出す
-      # これは画像をコピーしないため高速
-      # ただし、切り出した画像に書き込むと元画像にも書き込まれるので注意
-      # [y, x] の順番なので注意
-      splitImage = img[top:bottom, left:right]
-      column.append(splitImage)
-    images.append(column)
-  return images
+    images = []
+    # shapeで画像サイズを取れるが、[高さ, 幅, 3]の順番になっているので注意
+    fullWidth = img.shape[1]
+    fullHeight = img.shape[0]
+    splitWidth = fullWidth / columns
+    splitHeight = fullHeight / rows
+    for i in range(columns):
+      column = []
+      for j in range(rows):
+        left = splitWidth * i
+        right = left + splitWidth
+        top = splitHeight * j
+        bottom = top + splitHeight
+        # 画像を切り出す
+        # これは画像をコピーしないため高速
+        # ただし、切り出した画像に書き込むと元画像にも書き込まれるので注意
+        # [y, x] の順番なので注意
+        splitImage = img[top:bottom, left:right]
+        column.append(splitImage)
+      images.append(column)
+    return images
 
 # 色を比較する
 def compareColor(a, b):
-  # 各要素の差の二乗は簡単に書ける。
-  # -や*は、配列の各要素それぞれに対して実行される。
-  # np.sumで合計を求める。
-  return math.sqrt(np.sum((a-b)*(a-b)))
+    # 各要素の差の二乗は簡単に書ける。
+    # -や*は、配列の各要素それぞれに対して実行される。
+    # np.sumで合計を求める。
+    return math.sqrt(np.sum((a-b)*(a-b)))
 
 # 画像の辺を比較する
 # flag=Trueの場合、aの右にbを置く
 # flag=Falseの場合、aの下にbを置く
 def compare(imgA, imgB, flag):
-  width = imgA.shape[1]
-  height = imgA.shape[0]
-  if flag:
-    # 横に比較する場合
-    lineA = imgA[0:height, width - 1]
-    lineB = imgB[0:height, 0]
-  else:
-    # 縦に比較する場合
-    lineA = imgA[height - 1, 0:width]
-    lineB = imgB[0, 0:width]
-  #difference = 0.0
-  #for i in range(len(lineA)):
-  #  difference += compareColor(lineA[i], lineB[i])
-  difference = np.sum(np.sqrt(np.sum((lineA-lineB)*(lineA-lineB), axis=1)))
-  return difference / len(lineA)
+    width = imgA.shape[1]
+    height = imgA.shape[0]
+    if flag:
+      # 横に比較する場合
+      lineA = imgA[0:height, width - 1]
+      lineB = imgB[0:height, 0]
+    else:
+      # 縦に比較する場合
+      lineA = imgA[height - 1, 0:width]
+      lineB = imgB[0, 0:width]
+    #difference = 0.0
+    #for i in range(len(lineA)):
+    #  difference += compareColor(lineA[i], lineB[i])
+    difference = np.sum(np.sqrt(np.sum((lineA-lineB)*(lineA-lineB), axis=1)))
+    return difference / len(lineA)
 
 def sortWorst(dic):
-  #averages = {}
-  #for k, v in dic.items():
-  #  # 平均を求める
-  #  avg = 0.0
-  #  for candicate in v:
-  #    avg += candicate[1]
-  #  avg = avg / len(v)
-  #  averages[k] = avg
-  #return sorted(averages.items(), key=lambda a: a[1])
-  minItems = {}
-  for k, v in dic.items():
-    minValue = min(v, key=lambda a: a[1])
-    minItems[k] = minValue[1]
-  return sorted(minItems.items(), key=lambda a: a[1])
+    #averages = {}
+    #for k, v in dic.items():
+    #  # 平均を求める
+    #  avg = 0.0
+    #  for candicate in v:
+    #    avg += candicate[1]
+    #  avg = avg / len(v)
+    #  averages[k] = avg
+    #return sorted(averages.items(), key=lambda a: a[1])
+    minItems = {}
+    for k, v in dic.items():
+      minValue = min(v, key=lambda a: a[1])
+      minItems[k] = minValue[1]
+    return sorted(minItems.items(), key=lambda a: a[1])
 
 def firstSame(listA, listB, key=lambda a: a):
-  bSet = {key(b) for b in listB}
-  for a in listA:
-    if key(a) in bSet:
-      for b in listB:
-        if key(a) == key(b):
-          return a
-  return None
+    bSet = {key(b) for b in listB}
+    for a in listA:
+      if key(a) in bSet:
+        for b in listB:
+          if key(a) == key(b):
+            return a
+    return None
 
 def findRightBottom(resultW, resultH):
-  lst = []
-  for k, v in resultW.items():
-    minValueW = min(v, key=lambda a: a[1])[1]
-    minValueH = min(resultH[k], key=lambda a: a[1])[1]
-    lst.append((k, minValueW * minValueH))
-  lst.sort(key=lambda a: a[1])
-  lst.reverse()
-  return lst
+    lst = []
+    for k, v in resultW.items():
+      minValueW = min(v, key=lambda a: a[1])[1]
+      minValueH = min(resultH[k], key=lambda a: a[1])[1]
+      lst.append((k, minValueW * minValueH))
+    lst.sort(key=lambda a: a[1])
+    lst.reverse()
+    return lst
 
 
 def createArray(width, height):
-  array = []
-  for x in range(width):
-    column = []
-    for y in range(height):
-      column.append(None)
-    array.append(column)
-  return array
+    array = []
+    for x in range(width):
+      column = []
+      for y in range(height):
+        column.append(None)
+      array.append(column)
+    return array
 
 # [left, right, top, bottom]
 # -1は範囲外
 # Noneは未設定
 def getNeighbours(x, y, array):
-  width = len(array)
-  height = len(array[0])
-  result = [-1, -1, -1, -1]
-  if x - 1 >= 0:
-    result[0] = array[x - 1][y]
-  if x + 1 < width:
-    result[1] = array[x + 1][y]
-  if y - 1 >= 0:
-    result[2] = array[x][y - 1]
-  if y + 1 < height:
-    result[3] = array[x][y + 1]
-  return result
+    width = len(array)
+    height = len(array[0])
+    result = [-1, -1, -1, -1]
+    if x - 1 >= 0:
+      result[0] = array[x - 1][y]
+    if x + 1 < width:
+      result[1] = array[x + 1][y]
+    if y - 1 >= 0:
+      result[2] = array[x][y - 1]
+    if y + 1 < height:
+      result[3] = array[x][y + 1]
+    return result
 
 def firstSameN(lists, key=lambda a: a):
-  #bSet = {key(b) for b in listB}
-  sets = [set(map(key, lst)) for lst in lists]
-  for a in lists[0]:
-    if all([(key(a) in s) for s in sets]):
-      return a
-  return None
+    #bSet = {key(b) for b in listB}
+    sets = [set(map(key, lst)) for lst in lists]
+    for a in lists[0]:
+      if all([(key(a) in s) for s in sets]):
+        return a
+    return None
 
 # リストの各要素どうしを足す
 def arrayAdd(xs, ys):
-  return map(lambda x, y: x + y, xs, ys)
+    return map(lambda x, y: x + y, xs, ys)
 
 # aがn個入ったリストを作る
 def repeat(a, n):
-  return [a for i in range(n)]
+    return [a for i in range(n)]
 
 def average(xs):
-  return sum(xs) / len(xs)
+    return sum(xs) / len(xs)
 
 
 # TODO 2方向から攻める
 # 水平に動いた方が向く画像とそうでない画像を見極めて，移動を偏らせる?
 def sortImages2(resultAToBWidth, resultBToAWidth, resultAToBHeight, resultBToAHeight, startList, array):
-  values = createArray(splitColumns, splitRows)
-  width = len(array)
-  height = len(array[0])
-  # 左から，右から，上から，下から
-  tables = [resultAToBWidth, resultBToAWidth, resultAToBHeight, resultBToAHeight]
-  queue = deque([])
-  for pos, img, value in startList:
-    array[pos[0]][pos[1]] = img
-    values[pos[0]][pos[1]] = value
-    queue.append(pos)
-  while len(queue) != 0:
-    (x, y) = queue.popleft()
-    thisImg = array[x][y]
-    neighbours = getNeighbours(x, y, array)
-    neighboursPosX = [x - 1, x + 1, x, x]
-    neighboursPosY = [y, y, y - 1, y + 1]
-    for i, neighbour in enumerate(neighbours):
-      if neighbour != -1:# and neighbour == None:
-        nextNeighbours = getNeighbours(neighboursPosX[i], neighboursPosY[i], array)
-        nextNeighboursPosX = map(lambda a: a + neighboursPosX[i], [-1, 1, 0, 0])
-        nextNeighboursPosY = map(lambda a: a + neighboursPosY[i], [0, 0, -1, 1])
-        newImgs = []
-        for j, img in enumerate(nextNeighbours):
-          if img != -1 and img != None:
-            newImg = tables[j][img][0]
-            newImgs.append((values[nextNeighboursPosX[j]][nextNeighboursPosY[j]], newImg))
-        if newImgs != []:
-          print "-----"
-          print newImgs
-          #array[neighboursPosX[i]][neighboursPosY[i]] = min(newImgs, key=lambda a: a[1])[0]
-          nextImg = min(newImgs, key=lambda a: a[1][1])
-          #newValue = average(map(lambda a: a[0], newImgs)) + nextImg[1][1]
-          #newValue = nextImg[0] + nextImg[1][1]
-          newValue = nextImg[0] # やっつけ!結局一番角らしいやつを採用してるだけ!
-          if array[neighboursPosX[i]][neighboursPosY[i]] == None or values[neighboursPosX[i]][neighboursPosY[i]] > newValue:
-            if values[neighboursPosX[i]][neighboursPosY[i]] > newValue:
-              print "rewrite %s to %s" % (values[neighboursPosX[i]][neighboursPosY[i]], newValue)
-            array[neighboursPosX[i]][neighboursPosY[i]] = nextImg[1][0]
-            values[neighboursPosX[i]][neighboursPosY[i]] = newValue
-            queue.append((neighboursPosX[i], neighboursPosY[i]))
-  print "values-----------"
-  print(values)
+    values = createArray(splitColumns, splitRows)
+    width = len(array)
+    height = len(array[0])
+    # 左から，右から，上から，下から
+    tables = [resultAToBWidth, resultBToAWidth, resultAToBHeight, resultBToAHeight]
+    queue = deque([])
+    for pos, img, value in startList:
+      array[pos[0]][pos[1]] = img
+      values[pos[0]][pos[1]] = value
+      queue.append(pos)
+    while len(queue) != 0:
+      (x, y) = queue.popleft()
+      thisImg = array[x][y]
+      neighbours = getNeighbours(x, y, array)
+      neighboursPosX = [x - 1, x + 1, x, x]
+      neighboursPosY = [y, y, y - 1, y + 1]
+      for i, neighbour in enumerate(neighbours):
+        if neighbour != -1:# and neighbour == None:
+          nextNeighbours = getNeighbours(neighboursPosX[i], neighboursPosY[i], array)
+          nextNeighboursPosX = map(lambda a: a + neighboursPosX[i], [-1, 1, 0, 0])
+          nextNeighboursPosY = map(lambda a: a + neighboursPosY[i], [0, 0, -1, 1])
+          newImgs = []
+          for j, img in enumerate(nextNeighbours):
+            if img != -1 and img != None:
+              newImg = tables[j][img][0]
+              newImgs.append((values[nextNeighboursPosX[j]][nextNeighboursPosY[j]], newImg))
+          if newImgs != []:
+            print "-----"
+            print newImgs
+            #array[neighboursPosX[i]][neighboursPosY[i]] = min(newImgs, key=lambda a: a[1])[0]
+            nextImg = min(newImgs, key=lambda a: a[1][1])
+            #newValue = average(map(lambda a: a[0], newImgs)) + nextImg[1][1]
+            #newValue = nextImg[0] + nextImg[1][1]
+            newValue = nextImg[0] # やっつけ!結局一番角らしいやつを採用してるだけ!
+            if array[neighboursPosX[i]][neighboursPosY[i]] == None or values[neighboursPosX[i]][neighboursPosY[i]] > newValue:
+              if values[neighboursPosX[i]][neighboursPosY[i]] > newValue:
+                print "rewrite %s to %s" % (values[neighboursPosX[i]][neighboursPosY[i]], newValue)
+              array[neighboursPosX[i]][neighboursPosY[i]] = nextImg[1][0]
+              values[neighboursPosX[i]][neighboursPosY[i]] = newValue
+              queue.append((neighboursPosX[i], neighboursPosY[i]))
+    print "values-----------"
+    print(values)
 
+# MARK: main
 
 if len(sys.argv) != 2:
   print "引数が間違っておるぞ!"
@@ -475,7 +476,7 @@ while  len(queue) != 0: #キューの長さ分くりかえすでー
             if selection_count < LIMIT_SELECTION and operations[0][0] != "S" :
                 #heappush(queue , (caliculate_cost(operations), Node(looking_node.board, (i, j)),("S%d%d"%(i,j),operations),selection_count+1))
                 selected_node = Node(looking_node.board, (i, j))
-                heappush(queue , (caliculate_cost(operations) + selection_h_star(distance_table, selected_node), selected_node,("S%d%d"%(i,j),operations),selection_count+1))
+                heappush(queue , (caliculate_cost(operations) + selection_h_star(i,j), selected_node,("S%d%d"%(i,j),operations),selection_count+1))
 
 
 print "出なかった"

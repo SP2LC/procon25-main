45682db653574a711bf98b9dedb356558641a82d
Taichi-Systems <Cubid_10411@me.com>
2014-09-19 19:55:46 +0900

    試合用複数アルゴリズム実装用

---
+++ cobra/communication.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import requests
+from requests.auth import HTTPDigestAuth
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+from StringIO import StringIO
+import sys
+import re
+import math
+
+DIGEST_USER = "sp2lc"
+DIGEST_PASS = "********"
+
+PRACTICE_USER = "sp2lc"
+PRACTICE_PASS = "********"
+
+def get_problem(problem_id,to_communication):
+	if to_communication == "sp2lc":
+		r = requests.get('http://sp2lc.salesio-sp.ac.jp/procon.php',params = {'probID' : problem_id}, auth=HTTPDigestAuth(DIGEST_USER, DIGEST_PASS))
+		if r.text == "error" :
+			print "server error"
+			exit()
+		return r.content
+	elif to_communication == "procon":
+		r = requests.get("http://192.168.11.220/problem/prob%02d.ppm" % int(problem_id))
+		return r.content
+        else:
+                # practice
+                r = requests.get("http://procon2014-practice.oknct-ict.org/problem/ppm/%d" % int(problem_id))
+                return r.content
+
+
+def post_answer(answer_string, time, version_string, problem_id,to_communication):
+	if to_communication == "sp2lc":
+		r = requests.post('http://sp2lc.salesio-sp.ac.jp/procon.php',data = {'answer_string' : answer_string , 'time' : time , 'version' : version_string, 'probID' : problem_id}, auth=HTTPDigestAuth(DIGEST_USER, DIGEST_PASS))
+		if r.text == "error" :
+			print "server error"
+			exit()
+		elif r.text == "ok" :
+			print "登録完了！"
+		print r.text
+		return r.text
+	elif to_communication == "procon":
+		r = requests.post('http://192.168.11.220/SubmitAnswer',data = {'playerid' : "1", 'problemid' : problem_id, 'answer' : answer_string})
+		print r
+		return r.text
+        else:
+                r = requests.post("http://procon2014-practice.oknct-ict.org/solve/json/%d" % int(problem_id), data={"username": PRACTICE_USER, "passwd": PRACTICE_PASS, "answer_text": answer_string})
+                print r.text
+                return r.text
+
+
+

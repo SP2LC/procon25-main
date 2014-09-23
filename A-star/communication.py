b58ab8380467d4d697cfe58b8786fa0e39559103
Taichi-Systems <Cubid_10411@me.com>
2014-09-23 18:04:29 +0900

    communicationのバグ修正

--- A-star/communication.py
+++ A-star/communication.py
@@ -27,9 +27,10 @@ def get_problem(masterIP):
 	if not(r.json() == None):
 		para = r.json()
 		sortedImages = make_problem(para['columns'],para['rows'])
-		for i in range(len(para['answer'][0])):
-			for j in range(len(para['answer'][1])):
+		for i in range(len(para['answer'])):
+			for j in range(len(para['answer'][0])):
 				sortedImages[i][j] = (para['answer'][i][j][0],para['answer'][i][j][1])
+		print sortedImages
 
 	return {'answer' : sortedImages , 'columns' : para['columns'], 'rows' : para['rows'], 'lim_select' : para['lim_select'], 'selection_rate' : para['selection_rate'], 'exchange_rate' : para['exchange_rate']}
 
